5d2b4eb5531d7c80a3e0747c195500dd4b696bb2
Kseniia Anishchenko <kseninavoi@gmail.com>
2016-08-19 20:15:07 -0700

    Добавила функцию

---
+++ src/pictures.js
@@ -0,0 +1,16 @@
+'use strict';
+
+window.pictures = [];
+window.picturesList = function(pics) {
+  window.pictures = pics;
+  console.log(window.pictures);
+};
+
+var createRequest = function(src, jsonCallBack) {
+  var url = src + '?callback=' + jsonCallBack;
+  var scriptElem = document.createElement('script');
+  scriptElem.src = url;
+  document.body.appendChild(scriptElem);
+};
+
+createRequest('http://localhost:1506/api/pictures', 'picturesList');
