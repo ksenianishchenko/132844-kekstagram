565667046aa38318f9732cec53d8744054d142d3
Kseniia Anishchenko <kseninavoi@gmail.com>
2016-08-22 12:29:57 -0700

    Добавила фотографии на страницу

--- src/pictures.js
+++ src/pictures.js
@@ -3,7 +3,9 @@
 window.pictures = [];
 window.picturesList = function(pics) {
   window.pictures = pics;
-  console.log(window.pictures);
+  window.pictures.forEach(function(photo) {
+    getPictureBlock(photo, picturesContainer);
+  });
 };
 
 var createRequest = function(src, jsonCallBack) {
@@ -13,4 +15,40 @@ var createRequest = function(src, jsonCallBack) {
   document.body.appendChild(scriptElem);
 };
 
+var filters = document.querySelector('.filters');
+filters.classList.add('hidden');
+
 createRequest('http://localhost:1506/api/pictures', 'picturesList');
+
+var pictureTemplate = document.querySelector('#picture-template');
+var picturesContainer = document.querySelector('.pictures');
+var pictureBlockToClone;
+
+if('content' in pictureTemplate) {
+  pictureBlockToClone = pictureTemplate.content.querySelector('.picture');
+} else {
+  pictureBlockToClone = pictureTemplate.querySelector('.picture');
+}
+
+var getPictureBlock = function(data, container) {
+  var pictureBlock = pictureBlockToClone.cloneNode(true);
+  pictureBlock.querySelector('.picture-comments').textContent = data.comments;
+  pictureBlock.querySelector('.picture-likes').textContent = data.likes;
+
+  var newPhoto = new Image(182, 182);
+  newPhoto.onload = function() {
+    newPhoto.src = data.url;
+  };
+  newPhoto.onerror = function() {
+    pictureBlock.classList.add('.picture-load-failure');
+  };
+  newPhoto.src = data.url;
+
+  var pictureStats = pictureBlock.querySelector('.picture-stats');
+  pictureBlock.insertBefore(newPhoto, pictureStats);
+
+  container.appendChild(pictureBlock);
+  return pictureBlock;
+};
+
+filters.classList.remove('hidden');
