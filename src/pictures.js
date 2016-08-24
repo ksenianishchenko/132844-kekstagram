c2755df02667e39d4cc664b193f3fbbb19eeea2f
Kseniia Anishchenko <kseninavoi@gmail.com>
2016-08-24 09:37:04 -0700

    Оставила только алгоритм, отрисовывающий модуль

--- src/pictures.js
+++ src/pictures.js
@@ -1,53 +1,23 @@
 'use strict';
 
-window.pictures = [];
-window.picturesList = function(pics) {
-  window.pictures = pics;
-  window.pictures.forEach(function(photo) {
-    getPictureBlock(photo, picturesContainer);
-  });
-};
+module.exports = (function() {
+  var createRequest = require('./load');
+  var getPictureBlock = require('./get-picture-block');
 
-var createRequest = function(src, jsonCallBack) {
-  var url = src + '?callback=' + jsonCallBack;
-  var scriptElem = document.createElement('script');
-  scriptElem.src = url;
-  document.body.appendChild(scriptElem);
-};
-
-var filters = document.querySelector('.filters');
-filters.classList.add('hidden');
-
-createRequest('http://localhost:1506/api/pictures', 'picturesList');
-
-var pictureTemplate = document.querySelector('#picture-template');
-var picturesContainer = document.querySelector('.pictures');
-var pictureBlockToClone;
-
-if('content' in pictureTemplate) {
-  pictureBlockToClone = pictureTemplate.content.querySelector('.picture');
-} else {
-  pictureBlockToClone = pictureTemplate.querySelector('.picture');
-}
-
-var getPictureBlock = function(data, container) {
-  var pictureBlock = pictureBlockToClone.cloneNode(true);
-  pictureBlock.querySelector('.picture-comments').textContent = data.comments;
-  pictureBlock.querySelector('.picture-likes').textContent = data.likes;
-
-  var newPhoto = new Image(182, 182);
-  var imgElement = pictureBlock.querySelector('img');
-  newPhoto.onload = function(evt) {
-    imgElement.src = evt.target.src;
-  };
-  newPhoto.onerror = function() {
-    pictureBlock.classList.add('.picture-load-failure');
+  window.pictures = [];
+  window.picturesList = function(pics) {
+    window.pictures = pics;
+    window.pictures.forEach(function(photo) {
+      getPictureBlock(photo, picturesContainer);
+    });
   };
 
-  newPhoto.src = data.url;
+  var filters = document.querySelector('.filters');
+  filters.classList.add('hidden');
 
-  container.appendChild(pictureBlock);
-  return pictureBlock;
-};
+  createRequest('http://localhost:1506/api/pictures', 'picturesList');
 
-filters.classList.remove('hidden');
+  var picturesContainer = document.querySelector('.pictures');
+
+  filters.classList.remove('hidden');
+})();
