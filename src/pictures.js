d731cbe8cbc7e70240b5acda7f4155110f0a556c
Kseniia Anishchenko <kseninavoi@gmail.com>
2016-08-29 09:45:32 -0700

    Подключила зависимость

--- src/pictures.js
+++ src/pictures.js
@@ -3,12 +3,16 @@
 module.exports = (function() {
   var createRequest = require('./load');
   var getPictureBlock = require('./get-picture-block');
+  var gallery = require('./gallery');
 
   window.pictures = [];
   window.picturesList = function(pics) {
     window.pictures = pics;
+    gallery.setPictures(pics);
+    var i = 0;
     window.pictures.forEach(function(photo) {
-      getPictureBlock(photo, picturesContainer);
+      getPictureBlock(photo, picturesContainer, i);
+      i++;
     });
   };
 
