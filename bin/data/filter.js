1bd41e6c5113f2cb1076a54fa286a63000c96796
Kseniia Anishchenko <kseninavoi@gmail.com>
2016-09-05 21:15:29 -0700

    Переписала функцию

--- bin/data/filter.js
+++ bin/data/filter.js
@@ -1,5 +1,15 @@
 'use strict';
 
 module.exports = function(list, filterID) {
+  switch(filterID) {
+    case 'filter-new':
+      return list.sort(function(a, b) {
+        return b.created - a.created;
+      });
+    case 'filter-discussed':
+      return list.sort(function(a, b) {
+        return b.comments - a.comments;
+      });
+  }
   return list;
 };
