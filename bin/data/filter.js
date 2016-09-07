6a15375ce8b5ddcc8b6169c46972f4c503e466fb
Kseniia Anishchenko <kseninavoi@gmail.com>
2016-09-07 11:23:01 -0700

    Фильтрация за три дня

--- bin/data/filter.js
+++ bin/data/filter.js
@@ -3,6 +3,9 @@
 module.exports = function(list, filterID) {
   switch (filterID) {
     case 'filter-new':
+      list = list.filter(function(date) {
+        return new Date() - Date.parse(date.created) < 259200000;
+      });
       return list.sort(function(a, b) {
         return b.created - a.created;
       });
